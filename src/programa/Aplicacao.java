7873513afb1f1b4e35310902b4810433f6b3f1f8
Ademivaldo <coisaatoa2.0@gmail.com>
2022-02-27 09:56:03 -0300

    projeto em andamento

---
+++ src/programa/Aplicacao.java
@@ -0,0 +1,57 @@
+package programa;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Aplicacao{
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+       
+    System.out.println("Projeto de urna roubada: ");
+
+    System.out.println("Quantos candidatos? ");
+    Integer totCandidatos = sc.nextInt();
+    sc.nextLine();
+    ArrayList[][] candidados = new ArrayList[totCandidatos][2];
+
+    for(int i = 0; i<totCandidatos; i++){
+        System.out.print("Digite o nome do participante e o codigo: ");
+        String nome = sc.nextLine();
+        String codigo = sc.nextLine();
+     
+       
+    }
+    System.out.println("Qual participante quer que ganhe?");
+    for(int i = 0; i < candidatos.length; i++){
+        System.out.println("candidato " + candidatos[i] + " digite " + i);
+        System.out.println("por quantos?: ");
+        diferenca1 = sc.nextInt();
+        sc.nextLine();
+    }
+   
+    System.out.print(" :: ");
+    int campeao = sc.nextInt();
+    sc.nextLine();
+
+    System.out.println("votação: ");
+    System.out.println("Quantos vão votar?");
+    int participantes = sc.nextInt();
+    int eleitores[] = new int[participantes];
+
+    switch(campeao){
+        case 0:
+        for(int i = 0; i<eleitores.length ; i++){
+        System.out.println("Digite seu nome e o codigo do candidato: ");
+        String nomeELei = sc.nextLine();
+        int codigo = sc.nextInt();
+        if(codigo == 0){
+        candidatos[0] = ;
+            
+        }
+    
+        }
+    }
+    
+
+    sc.close();
+    }
+}
---
+++ src/programa/Candidatos.java
@@ -0,0 +1,13 @@
+package programa;
+
+public class Candidatos {
+     public String nome;    
+     public int codigo;
+
+
+
+public Candidatos(String nome, int codigo){
+    this.nome = nome;
+    this.codigo = codigo;
+}
+}
